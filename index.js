d9a5d65c05dc1e11780904d8147cfef2b9d6ec4b
bradmwong <bradmwong@hotmail.com>
2023-09-25 10:41:36 -0700

    Added Function to Update Errors Column

--- index.js
+++ index.js
@@ -1,23 +1,22 @@
 const PORT = 8000;
 const express = require("express");
-const ejsMate = require('ejs-mate');
+const ejsMate = require("ejs-mate");
 const bodyParser = require("body-parser");
 const app = express();
-const cors = require('cors');
+const cors = require("cors");
 
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + "/public"));
 
-app.engine('ejs', ejsMate);
-app.set('view engine', 'ejs');
+app.engine("ejs", ejsMate);
+app.set("view engine", "ejs");
 
-const { fetchLinks } = require('./models/fetchSiteMapLinks');
-const { assignErrors } = require('./models/assignErrors');
+const { fetchLinks } = require("./models/fetchSiteMapLinks");
+const { assignErrors } = require("./models/assignErrors");
 
-app.get('/', (req, res) => {
-
+app.get("/", (req, res) => {
     // Initialize Variables
     const siteMapUrl = "";
     const links = {};
@@ -25,39 +24,37 @@ app.get('/', (req, res) => {
     res.render("home", { siteMapUrl, links });
 });
 
-app.post('/', async (req, res) => {
-
+app.post("/", async (req, res) => {
     const siteMapUrl = req.body.siteMapUrl;
-    let linksDict = req.body.linksDict || await fetchLinks(siteMapUrl) || {};
-
+    let linksDict = req.body.linksDict || (await fetchLinks(siteMapUrl)) || {};
 
     let page = req.body.page || 1;
     let quantity = 5;
 
     linksDict = assignErrors(linksDict, page, quantity);
-    console.log(linksDict);
-    
+    // console.log(linksDict);
+
     res.render("home", { siteMapUrl, linksDict, page, quantity });
 });
 
+app.post("/nextPage", async (req, res) => {
+    const incompleteURLs = req.body.incompleteURLs;
+    let completedURLsDict = {};
+    console.log(incompleteURLs);
+    for (let i = 0; i < incompleteURLs.length; i++) {
+        completedURLsDict[incompleteURLs[i]] = ["a", "b", "c", i];
+    }
 
-app.post('/nextPage', async (req, res) => {
-
-    const updatedText = `NEXT BUTTON PRESSED ${req.body.incompleteURLs}`;
-    res.send(updatedText);
-
+    res.send(completedURLsDict);
 });
 
-app.post('/previousPage', async (req, res) => {
-
-    const updatedText = `PREVIOUS BUTTON PRESSED ${req.body.incompleteURLs}`;
+app.post("/previousPage", async (req, res) => {
+    const updatedText = `${req.body.incompleteURLs}`;
     res.send(updatedText);
-
 });
 
-
-app.get('/test', function (req, res) {
-    res.sendFile(__dirname + '/public/page.html');
-})
+app.get("/test", function (req, res) {
+    res.sendFile(__dirname + "/public/page.html");
+});
 
 app.listen(PORT, () => console.log(`server running on PORT ${PORT}`));
