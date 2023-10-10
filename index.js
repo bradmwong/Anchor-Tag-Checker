363070d9772bddd46b0349d3c4b11a79953bd16f
bradmwong <bradmwong@hotmail.com>
2023-10-09 23:43:08 -0700

    Added fallback for non-SiteMap links

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ const { assignErrors } = require("./models/assignErrors");
 
 
 app.get("/", (req, res) => {
-    // Initialize Variables
+
     const siteMapUrl = "";
     const links = {};
 
@@ -27,22 +27,32 @@ app.get("/", (req, res) => {
 });
 
 app.post("/", async (req, res) => {
+
     const siteMapUrl = req.body.siteMapUrl;
     let linksDict = req.body.linksDict || (await fetchLinks(siteMapUrl)) || {};
-    
     let page = req.body.page || 1;
     let quantity = 5;
 
-    linksDict = await assignErrors(linksDict, page, quantity);
-    console.log(await fetchAnchorElementsWithoutIDs("https://developers.google.com/maps/documentation/javascript/reference/3.53/places-autocomplete-service/"))
+    // Assign URL to object as a single entry if no SiteMap URLs found
+    if (Object.keys(linksDict).length <= 0) {
+        linksDict[siteMapUrl] = [];
+        quantity = 1;
+    };
 
+    // Check first page for errors
+    linksDict = await assignErrors(linksDict, page, quantity);
+    console.log(linksDict);
+
+    // Reload home page with updated variables
     res.render("home", { activeTab: 'home', siteMapUrl, linksDict, page, quantity });
 });
 
 
 app.post("/changePage", async (req, res) => {
+
     const incompleteURLs = req.body.incompleteURLs;
     let completedURLsDict = {};
+
     for (let i = 0; i < incompleteURLs.length; i++) {
         completedURLsDict[incompleteURLs[i]] = await fetchAnchorElementsWithoutIDs(incompleteURLs[i]);
     }
